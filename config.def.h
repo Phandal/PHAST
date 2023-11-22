44895c1a40335fae5bcd2b9af658e5b71bd6468c
Phandal <bfeld.111@gmail.com>
2023-11-22 08:19:43 -0600

    updated nerd fonts

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint-true";
-static char *font = "Iosevka Nerd Font Mono:pixelsize=14:antialias=true:autohint-true";
+static char *font = "Iosevka NFM:pixelsize=14:antialias=true:autohint-true";
 static int borderpx = 2;
 
 /*
